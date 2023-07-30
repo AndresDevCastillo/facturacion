6bd97a3e47dde732146ffd3a17367a6eaa29b0f7
Andres Castillo <115095588+AndresDevCastillo@users.noreply.github.com>
2023-07-30 15:34:31 -0500

    se creo el proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hola</h1>
+  <NarvarHome />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NarvarHome from '@/components/Narvar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NarvarHome,
   }
 }
 </script>
---
+++ src/components/Narvar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card>
+        <v-layout>
+            <v-navigation-drawer expand-on-hover :rail="rail" theme="dark">
+                <v-list>
+                    <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Andres Peréz"
+                        subtitle="AndresP07@gmailcom"></v-list-item>
+                </v-list>
+
+                <v-divider></v-divider>
+
+                <v-list density="comfortable" nav>
+                    <v-list-item prepend-icon="mdi-folder" title="Facturación" value="myfiles"></v-list-item>
+                    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+                    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+
+                </v-list>
+            </v-navigation-drawer>
+
+            <v-main style="height: 750px"></v-main>
+        </v-layout>
+    </v-card>
+</template>
+  
+<script>
+export default {
+    name: 'NarvarHome',
+
+}
+</script>
