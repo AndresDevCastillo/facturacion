afe8b5c1fc912a9f50d1f9a433adb3a1b9ba2dd3
Andres Castillo <115095588+AndresDevCastillo@users.noreply.github.com>
2023-07-30 17:03:45 -0500

    se agregaron los icons

--- src/components/Narvar.vue
+++ src/components/Narvar.vue
@@ -1,7 +1,21 @@
 <template>
     <v-card>
         <v-layout>
-            <v-navigation-drawer expand-on-hover :rail="rail" theme="dark">
+            <v-app-bar theme="dark" prominent>
+                <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+                <v-toolbar-title>My files</v-toolbar-title>
+
+                <v-spacer></v-spacer>
+
+                <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+                <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+                <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+            </v-app-bar>
+            <v-navigation-drawer theme="dark" v-model="drawer" location="left">
+
                 <v-list>
                     <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Andres Peréz"
                         subtitle="AndresP07@gmailcom"></v-list-item>
@@ -10,13 +24,17 @@
                 <v-divider></v-divider>
 
                 <v-list density="comfortable" nav>
-                    <v-list-item prepend-icon="mdi-folder" title="Facturación" value="myfiles"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-                    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+                    <v-list-item prepend-icon="mdi mdi-folder" title="Facturación" value="myfiles"
+                        class="items-bar"></v-list-item>
+                    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"
+                        class="items-bar"></v-list-item>
+                    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred" class="items-bar"></v-list-item>
                 </v-list>
             </v-navigation-drawer>
 
-            <v-main style="height: 750px"></v-main>
+            <v-main style="height: 750px">
+
+            </v-main>
         </v-layout>
     </v-card>
 </template>
@@ -24,6 +42,17 @@
 <script>
 export default {
     name: 'NarvarHome',
-
+    data: () => ({
+        drawer: true,
+        group: null,
+    })
 }
 </script>
+
+<style>
+.items-bar>.v-list-item__content>.v-list-item-title {
+    font-size: 1.4rem;
+    padding-top: 10px;
+    text-align: left;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 // Vuetify
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import createVuetify from './plugins/vuetify'
 
-const vuetify = createVuetify({
-    components,
-    directives,
-})
 
-createApp(App).use(vuetify).mount('#app')
+
+createApp(App).use(createVuetify).mount('#app')
