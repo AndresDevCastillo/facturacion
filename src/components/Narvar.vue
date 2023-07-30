b8aa18b296f6dcff94ef611758e7197dc01142f3
Andres Castillo <115095588+AndresDevCastillo@users.noreply.github.com>
2023-07-30 15:48:04 -0500

    se añadio vue router

--- src/components/Narvar.vue
+++ src/components/Narvar.vue
@@ -13,7 +13,6 @@
                     <v-list-item prepend-icon="mdi-folder" title="Facturación" value="myfiles"></v-list-item>
                     <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
                     <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-
                 </v-list>
             </v-navigation-drawer>
 
