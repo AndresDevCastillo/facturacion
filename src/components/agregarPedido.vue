641be38a540228999e3a403598b7c2c75c03aad5
Luis-devs <luismonteria2001@gmail.com>
2023-08-17 21:23:56 -0500

    Agregar pedido terminado

--- src/components/agregarPedido.vue
+++ src/components/agregarPedido.vue
@@ -1,20 +1,37 @@
 <template>
     <div>
         <v-card class="ma-3">
+            <v-row no-gutters class="flex-column px-2">
+                <v-col cols="12">
+                    <h2>Buscar cliente</h2>
+                </v-col>
+                <v-col cols="5">
+                    <v-autocomplete persistent-hint density="compact" return-object
+                        label="Buscar nombre del cliente"
+                        :items="clientes" item-title="nombre" variant="outlined" hint="Si no lo encuentra puede escribirlo en el campo de abajo" v-model="buscandoCliente" @update:modelValue="clienteSeleccionado"></v-autocomplete>
+                </v-col>
+            </v-row>
             <v-form ref="form">
                 <v-container>
                     <v-row>
                         <v-col cols="12" md="3">
-                            <v-autocomplete clearable
-                                label="Nombre del cliente"
-                                :items="clientes" variant="outlined" v-model="form.nombre" @update:search="buscarCliente"></v-autocomplete>
-                            <!-- <v-text-field v-model="form.nombre" label="Nombre del cliente" placeholder="Pepito pérez" required
-                                variant="outlined"></v-text-field> -->
+                            <v-text-field v-model="this.form.nombre" label="Nombre del cliente" placeholder="Pepito pérez" required
+                                variant="outlined" @keyup="quitarIdCliente"></v-text-field>
                         </v-col>
                         <v-col cols="12" md="3">
-                            <v-select v-model="add" :items="products" label="Productos" item-title="name" return-object=""
+                            <v-autocomplete :items="this.ubicaciones" item-title="ubicacion" item-value="id" v-model="this.form.ubicacion" label="Mesa" placeholder="Escoja mesa" required
+                                variant="outlined"></v-autocomplete>
+                        </v-col>
+                        <v-col cols="12" md="3">
+                            <v-select :items="this.meseros" item-title="nombre" item-value="id" v-model="this.form.mesero" label="Mesero" placeholder="Escoja mesero" required
+                                variant="outlined"></v-select>
+                        </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="12" md="3">
+                            <v-autocomplete v-model="add" :items="productos" label="Productos" no-data-text="Sin productos" item-title="nombre" return-object
                                 placeholder="Escoja producto" required variant="outlined">
-                            </v-select>
+                            </v-autocomplete>
                         </v-col>
 
                         <v-col cols="12" md="3">
@@ -28,182 +45,152 @@
                 </v-container>
             </v-form>
         </v-card>
-        <v-table fixed-header fixed-footer height="400" v-if="form.compras.length != 0">
+        <v-table class="pb-5" fixed-header fixed-footer height="360" v-if="form.compras.length != 0">
             <thead style="z-index: 999999;">
                 <tr>
                     <th class="text-left">
-                        Name
-                    </th>
-                    <th class="text-left">
-                        Descripcion
-                    </th>
-                    <th class="text-left">
-                        Descuento
+                        Producto
                     </th>
                     <th class="text-left">
                         Cantidad
                     </th>
                     <th class="text-left">
-                        Precio Neto
+                        Precio
                     </th>
                     <th>Acción</th>
-
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(item, index) in form.compras" :key="item.name">
-                    <td class="text-left">{{ item.name }}</td>
-                    <td class="text-left">{{ item.descripcion }}</td>
-                    <td class="text-left">{{ item.descuento }}</td>
+                    <td class="text-left">{{ item.nombre }}</td>
                     <td class="text-left">{{ item.cantidad }}</td>
                     <td class="text-left">{{ item.precio }}</td>
-                    <td><v-btn elevation="4" @click="eliminarCompra(index)">eliminar</v-btn> </td>
+                    <td><v-btn elevation="4" @click="eliminarCompra(index)" color="red">eliminar</v-btn> </td>
                 </tr>
             </tbody>
-            <tfoot>
+            <tfoot style="z-index: 999999;">
                 <tr>
-                    <td class="text-left footer-table" colspan="2">Pago : {{ total.total }}</td>
-                    <td class="text-left footer-table" colspan="1">{{ total.descuento }}</td>
-                    <td class="text-left footer-table" colspan="1">{{ total.cantidad }}</td>
-                    <td class="text-left footer-table" colspan="2">{{ total.neto }}</td>
+                    <td class="text-left footer-table" colspan="2"><strong> Pagar : {{ pagarNeto }}</strong></td>
+                    <td colspan="2" class="text-center">
+                        <v-btn elevation="4" @click="guardarPedido()" color="primary">Guardar pedido</v-btn>
+                    </td>
                 </tr>
             </tfoot>
         </v-table>
-        <facturaComponent :dialog="dialog" :datos="form" @cerrarDialogo="dialog = false" />
-        <ticketComponent :dialog="dialogTicket" @cerrarDialogoTicket="dialogTicket = false"></ticketComponent>
-        <ticketCocineroComponent :dialog2="dialogTicketCocinero" @cerrarDialogoTicketCocinero="dialogTicketCocinero = false"></ticketCocineroComponent>
     </div>
 </template>
   
 <script>
 import Swal from 'sweetalert2';
-import facturaComponent from '../components/factura.vue';
-import ticketComponent from '../components/ticket.vue';
-import ticketCocineroComponent from '../components/ticketCocinero.vue';
+import axios from 'axios';
 export default {
     name: 'agregarPedidoComponent',
     components: {
-        facturaComponent,
-        ticketComponent,
-        ticketCocineroComponent
     },
     data: () => ({
-        clientes: ['Luis', 'Juan', 'José'],
+        buscandoCliente: null,
+        clientes: [{ id: 1, nombre: 'Luis' }, { id: 2, nombre: 'Juan' }, { id: 3, nombre: 'José' }],
         bCliente: null,
         valid: null,
-        dialog: false,
-        dialogTicket: false,
-        dialogTicketCocinero: true,
         add: null,
         cantidad: null,
-        drawer2: true,
-        group: null,
         pagos: ['Efectivo', 'Tarjeta Credito', 'Tarjeta Debito', 'Transferencia'],
         form: {
+            idCliente: null,//Si va vacío es porque no existe en la bd
             nombre: null,
-            cedula: null,
-            pago: null,
             compras: [],
             ubicacion: null,
             mesero: null,
-            domiciliario: null,
-            direccion: null,
-            numero: null,
-            correo: null,
-            descuento: 0,
 
         },
-        meseros: ['Javier Gómez', 'María Rodríguez', 'Carlos Pérez', 'Ana Martínez', 'Luis Ramírez'],
+        meseros: [{ id: 1, nombre: 'Javier Gómez' }, { id: 2, nombre: 'María Rodríguez' }, { id: 3, nombre: 'Carlos Pérez' }, { id: 4, nombre: 'Ana Martínez' }, { id: 5, nombre: 'Luis Ramírez' }],
         domiciliarios: ['Carlos Lopéz', 'Daver Mercado', 'José Armando Ortega'],
-        ubicaciones: ['Domicilio', 'Recogido en persona', 'Restaurante'],
-        defect: 'No hay medios disponibles',
-        products: [{
-            name: "Manzanas",
-            descripcion: "Frescas manzanas rojas.",
-            precio: 1000,
-            descuento: 30,
-        },
-        {
-            name: "Arroz",
-            descripcion: "Arroz de grano largo, 1kg.",
-            precio: 1800,
-            descuento: 0,
-        },
-        {
-            name: "Leche",
-            descripcion: "Leche semidesnatada, 1L.",
-            precio: 2200,
-            descuento: 0,
-        },
-        {
-            name: "Pan",
-            descripcion: "Pan blanco recién horneado.",
-            precio: 500,
-            descuento: 900,
-        },
-        {
-            name: "Huevos",
-            descripcion: "Huevos frescos, docena.",
-            precio: 200,
-            descuento: 700,
-        },],
-        total: {
-            total: null,
-            descuento: null,
-            cantidad: null,
-            neto: null
-        },
+        ubicaciones: [{ id: 1, ubicacion: 'Mesa 1' }, { id: 2, ubicacion: 'Mesa 2' }, { id: 3, ubicacion: 'Mesa 3' }],
+
+        productos: [],
 
     }),
+
     methods: {
         addCompra() {
             this.cantidad = parseFloat(this.cantidad);
-            console.log(typeof (this.cantidad))
             if (this.cantidad <= 0 || !Number.isInteger(this.cantidad) || this.add == null) {
                 return Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
                     text: 'Ingrese la cantidad correctamente'
-                })
+                });
             }
-            this.add.precio = this.add.precio * this.cantidad;
-            this.add.descuento = this.add.descuento * this.cantidad;
-            this.add.cantidad = parseInt(this.cantidad);
-            this.form.compras.push(this.add);
+            this.form.compras.push({ id: this.add.id, nombre: this.add.nombre, precio: this.add.precio, cantidad: this.cantidad });
             this.add = null;
             this.cantidad = null;
-            this.calcularMonto();
-
         },
         eliminarCompra(posicion = null) {
             this.form.compras.splice(posicion, 1);
-            this.calcularMonto();
 
         },
-        calcularMonto() {
-
-            let descuentoTemp = 0;
-            let cantidadTemp = 0;
-            let netoTemp = 0;
-            for (let item of this.form.compras) {
-                console.log(item);
-                descuentoTemp += item.descuento;
-                cantidadTemp += item.cantidad;
-                netoTemp += item.precio;
+        guardarPedido() {
+            if (this.form.nombre == null || this.form.nombre.length == 0) {
+                return Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'El nombre del cliente no puede estar vacío',
+                    timer: 1500,
+                    showConfirmButton: false,
+                });
             }
-            this.total.total = netoTemp - descuentoTemp;
-            this.total.descuento = descuentoTemp;
-            this.total.neto = netoTemp;
-            this.total.cantidad = cantidadTemp;
-        },
-        generarFactura() {
-            this.dialog = true;
+            //Envio al backend
+            console.log(this.form);
         },
         buscarCliente(busqueda) {
             const indexCliente = this.clientes.findIndex(cliente => cliente === busqueda);
             if (indexCliente == -1) {
-                this.clientes.push(busqueda);
+                this.form.nombre = busqueda;
             }
+        },
+        quitarIdCliente() {
+            this.form.idCliente = null;
+        }
+        ,
+        clienteSeleccionado(cliente) {
+            this.form.nombre = cliente.nombre;
+            this.form.idCliente = cliente.id;
+        },
+        async obtenerProductos() {
+            let prod;
+            await axios.get(`${process.env.VUE_APP_API_URL}/producto`).then((resp) => {
+                if (resp.data.length > 0) {
+                    resp.data.sort((a, b) => a.nombre.localeCompare(b.nombre));
+                }
+                prod = resp.data;
+            });
+            console.log(prod);
+            this.productos = [{ nombre: 'Pepe' }, { nombre: 'Pepito' }];
+        }
+    },
+    watch: {
+        /*  buscandoCliente() {
+             this.form.nombre = this.buscandoCliente;
+         }, */
+        /* nombreCliente() {
+            
+        } */
+    },
+    async created() {
+        await axios.get(`${process.env.VUE_APP_API_URL}/producto`).then((resp) => {
+            /* if (resp.data.length > 0) {
+                resp.data.sort((a, b) => a.nombre.localeCompare(b.nombre));
+            } */
+            this.productos = resp.data;
+        });
+    },
+    computed: {
+        pagarNeto() {
+            let pagoN = 0;
+            for (let item of this.form.compras) {
+                pagoN += parseInt(item.cantidad * item.precio);
+            }
+            return pagoN;
         }
     },
     mounted() {
